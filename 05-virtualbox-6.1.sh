a17c21937c13e064aece2eaf9d6b4a61b92d6987
herbertlobo <herbertlobo@outlook.com>
2021-02-16 17:05:41 -0300

    virtualbox-6.1 no debian

---
+++ 05-virtualbox-6.1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# virtual  box 6.1
+sudo apt install virtualbox-6.1
+
+
+#Erro no modulo do Kernel, VirtualBox, muito comum após instalação de um kernel novo.
+#*Esses comandos valem para Ubuntu, Linux Mint Debian e derivados.
+#(você pode adapta-los para sua distro, procurando por esses complementos no gerenciador de pacotes padrão do seu sistema, caso seja um Fedora etc).
+
+
+sudo apt-get install build-essential module-assistant
+
+sudo m-a prepare
+
+#*Depois compile o driver
+sudo /sbin/vboxconfig
+#ou 
+sudo vboxconfig 
+
